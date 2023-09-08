a5b5e5ddbb6923da7d9323838ad5dda2fca65a3d
Sinabb <86355292+Sinabb@users.noreply.github.com>
2023-09-08 22:45:56 +0900

    XML 자료 PDF 복습

---
+++ DogStudy.cpp
@@ -0,0 +1,1 @@
+#include "DogStudy.h"
---
+++ Xmlstudy.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <vector>
+
+class Sprite
+{
+public:
+	std::string n;
+	int x;
+	int y;
+	int w;
+	int h;
+};
+
+void LoadXML(const char* filename, std::vector<Sprite>& sprites)
+{
+	sprites.clear();
+
+	std::ifstream file(filename, std::ifstream::binary);
+	std::string line;
+	std::regex pattern("\"([^\"]*)\"");
+
+}
